46b0b25e83d5f2ff9811883371e7f771d5ca968d
Pavel Pesherov <43248861+XDshnik@users.noreply.github.com>
2021-03-26 16:44:25 +0300

    feat: add code that don't pass tricky and very tricky tests but other is fine
    NEED FIX!

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+
+  let string = 'Unable to determine the time of year!';
+  if (typeof(date) === "undefined" ) {
+    string = 'Unable to determine the time of year!';
+    return string;
+  }
+  if (!(date instanceof Date)) {
+    return string;
+  }
+/* let month;
+  try {
+    month = date.getMonth();
+  } catch (error) {
+    console.log('error! ', date)
+  }
+  */
+let month = date.getMonth();
+
+  if (month === 11 || month === 0 || month === 1) {
+    string = 'winter';
+  } else if (month >= 2 && month <= 4) {
+      string = 'spring';
+    } else if (month >= 5 && month <= 7) {
+        string = 'summer';
+      } else if (month >= 8 && month <= 10) {
+          string = 'autumn';
+        }
+        console.log(string);
+  return string;
+}
