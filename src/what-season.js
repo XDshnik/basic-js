5c0c1d38c7af7e1e10fa12eb63d1378737a4d99b
Pavel Pesherov <43248861+XDshnik@users.noreply.github.com>
2021-03-26 17:19:59 +0300

    fix: refactor: add get-season functionality with needed checks

--- src/what-season.js
+++ src/what-season.js
@@ -2,32 +2,28 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
 
-  let string = 'Unable to determine the time of year!';
-  if (typeof(date) === "undefined" ) {
-    string = 'Unable to determine the time of year!';
-    return string;
+  if (date === undefined || date === null) {
+    if (date && typeof date.getMonth !== 'function') {
+      throw new Error();
+    }
+    return 'Unable to determine the time of year!';
   }
-  if (!(date instanceof Date)) {
-    return string;
+  
+  const month = date.getMonth();
+  if (typeof month !== 'number') {
+    return 'Unable to determine the time of year!'
   }
-/* let month;
-  try {
-    month = date.getMonth();
-  } catch (error) {
-    console.log('error! ', date)
-  }
-  */
-let month = date.getMonth();
-
+  
   if (month === 11 || month === 0 || month === 1) {
-    string = 'winter';
-  } else if (month >= 2 && month <= 4) {
-      string = 'spring';
-    } else if (month >= 5 && month <= 7) {
-        string = 'summer';
-      } else if (month >= 8 && month <= 10) {
-          string = 'autumn';
-        }
-        console.log(string);
-  return string;
+    return 'winter';
+  }
+  if (month >= 2 && month <= 4) {
+    return 'spring';
+  } 
+  if (month >= 5 && month <= 7) {
+    return 'summer';
+  } 
+  if (month >= 8 && month <= 10) {
+    return 'autumn';
+  }
 }
